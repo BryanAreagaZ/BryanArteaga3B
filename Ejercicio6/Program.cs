a5e7c58d0f3e9b161f64b0c0bb1559436b4d2c25
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 18:03:05 -0500

    Add files via upload

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+            Console.Write("ingrese la altura del rombo:");
+            c = int.Parse(Console.ReadLine());
+            for (a = 1; a <= c; a++)
+            {
+                for (b = 1; b <= c - a; b++)
+                    Console.Write(" ");
+                for (b = 1; b <= 2 * a - 1; b++)
+                    Console.Write("*");
+                Console.WriteLine();
+
+            }
+            for (a = c - 2; a >= 0; a--)
+            {
+                for (b = 1; b <= c - a - 1; b++)
+                    Console.Write(" ");
+                for (b = 0; b < 2 * a + 1; b++)
+                    Console.Write("*");
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
