ecb2fb6af435616db7f757e1e913118c0d128a6a
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 17:56:00 -0500

    Add files via upload

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Time, otro, TimeEx, Total = 525;
+            double Salario‬, SalarioEx;
+            do
+            {
+                Console.Write("Ingrese las horas trabajadas por los empleados :");
+
+                Time = int.Parse(Console.ReadLine());
+                if (Time <= 35)
+                {
+                    Salario = Time * 15;
+                }
+                else
+                {
+                    TimeEx = Time - 35;
+                    SalarioEx = TimeEx * 22;
+                    Salario = SalarioEx + Total;
+
+                }
+                Console.WriteLine("El salario total de los empleados es: " + Salario);
+                Console.WriteLine("Si desea ingresar otro salaio precione 1 caso contrario preione 0 :");
+                otro = int.Parse(Console.ReadLine());
+            }
+            while (otro == 1);
+
+            Console.WriteLine("El programa se ha cerrado Exitosamente");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
