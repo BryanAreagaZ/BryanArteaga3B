1d0a0d299506096d318c872c4b2a809cf9ee4825
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 17:27:47 -0500

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el numero del tamaño del cuadrado");
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 0 && n <= 50)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine(" ");
+                for (int i = 0; i < n - 2; i++)
+                {
+                    Console.Write(" *");
+                    for (int j = 0; j < n - 2; j++)
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(" *");
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(" *");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
