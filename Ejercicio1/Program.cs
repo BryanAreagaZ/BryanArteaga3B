9b702bcdf6f14b6c41ed4035e9586aacb66390f4
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 17:36:35 -0500

    Add files via upload

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant, num, mayor = 0;
+            Console.WriteLine("La cantidad de numeros que desea ingresar :");
+            cant = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cant; i++)
+            {
+                Console.Write("Escriba el numero ");
+                num = int.Parse(Console.ReadLine());
+                if (num > mayor)
+                {
+                    mayor = num;
+                }
+            }
+            Console.WriteLine("El numero mayor es: " + mayor);
+            Console.ReadKey();
+        }
+    }
+}
