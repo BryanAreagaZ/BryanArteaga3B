550986fad853c06cee0d0c6e5a18df9412bd919f
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 17:59:57 -0500

    Add files via upload

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("ingrese la altura de la piramide:");
+            c = int.Parse(Console.ReadLine());
+            for (a = 1; a <= c; a++)
+            {
+                for (b = 1; b <= c - a; b++)
+                    Console.Write(" ");
+                for (b = 1; b <= 2 * a - 1; b++)
+                    Console.Write("*");
+                Console.WriteLine(" ");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
