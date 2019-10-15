b9457a5e88580f28b0ad3a8e8a0acc41216aa616
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 18:18:43 -0500

    Add files via upload

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabla, var1 = 1, T = 0;
+            Console.Write("digite el numero de la tabla:");
+            tabla = int.Parse(Console.ReadLine());
+            for (var1 = 1; var1 <= 15; var1++)
+            {
+                T = var1 * tabla;
+                Console.WriteLine(+tabla + "x" + var1 + "=" + T);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
