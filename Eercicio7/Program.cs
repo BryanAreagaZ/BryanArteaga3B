49948b7294adbdfa275d7d519080492cef4a80e3
BryanAreagaZ <56612197+BryanAreagaZ@users.noreply.github.com>
2019-10-15 18:13:53 -0500

    Add files via upload

---
+++ Eercicio7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Eercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0, B = 101;
+            while (A < 100)
+            {
+                A = A + 1;
+                B = B - 1;
+                Console.WriteLine(+A + "  " + B);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
